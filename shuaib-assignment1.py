ced5249bfab8efb42346d8eb525b1e1907bf6d01
shuaib smarty <muhammedsuhaib873@gmail.com>
2019-02-24 12:25:37 +0530

    (files added)

--- shuaib-assignment1.py
+++ shuaib-assignment1.py
@@ -3,7 +3,7 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 example_sent = "can nltk remove the stopwords from this sample sentence-my name is shuaib"
-stop_words = set(stopwords.words(example_sent)
+stop_words = set(stopwords.words(example_sent))
 word_tokens = word_tokenize(example_sent)
 filtered_sentence = [w for w in word_tokens if not w in stopwords]
 filtered_sentence = []
